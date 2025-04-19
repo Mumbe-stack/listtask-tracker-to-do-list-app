e03759e59960a2de1525a239dafb5cc17ece9f77
Mercy <mercymumbe17@gmail.com>
2025-04-19 03:36:40 +0300

    updated app header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-
-import './App.css'
+import React, { useState } from 'react';
+// import TaskList from './components/TaskList';
+// import TaskForm from './components/TaskForm';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <div className="app">
+      <h1>TaskTrackr - To-Do List App</h1>
+      {/* <TaskForm addTask={addTask} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} editTask={editTask} /> */}
+    </div>
+  
     </>
   )
 }
